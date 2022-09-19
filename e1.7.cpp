5f58bebe0e2856d05c2757d80e22ac6233ee8466
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-19 19:08:55 -0400

    Create e1.7.cpp

---
+++ e1.7.cpp
@@ -0,0 +1,17 @@
+/*
+Author: Frida Mesa
+Course: CSCI-135
+Instructor: Genady Maryash
+Assignment: Homework E1.7
+Names 3 animals
+*/
+
+#include <iostream>
+using namespace std;
+int main()
+{
+cout << "dog"<< endl;
+cout << "cat"<< endl;
+cout << "fish"<< endl;
+return 0;
+}
